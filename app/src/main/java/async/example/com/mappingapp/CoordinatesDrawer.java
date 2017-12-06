0f7e484e011dcfcc15b333b6b5e6a94bc436d877
VakHack <vakrat@gmail.com>
2017-12-06 13:43:24 +0200

    Initial commit

---
+++ app/src/main/java/async/example/com/mappingapp/CoordinatesDrawer.java
@@ -0,0 +1,17 @@
+package async.example.com.mappingapp;
+
+import android.util.Log;
+
+import com.google.android.gms.maps.GoogleMap;
+
+/**
+ * Created by Administrator on 06/12/2017.
+ */
+
+public class CoordinatesDrawer {
+
+    public static void draw(GoogleMap map, int rawKmlFileId){
+
+        NavigationSaxHandler saxHandler = new NavigationSaxHandler();
+    }
+}
---
+++ app/src/main/java/async/example/com/mappingapp/NavigationDataSet.java
@@ -0,0 +1,45 @@
+package async.example.com.mappingapp;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+
+public class NavigationDataSet {
+
+    private ArrayList<Placemark> placemarks = new ArrayList<Placemark>();
+    private Placemark currentPlacemark;
+    private Placemark routePlacemark;
+
+    public String toString() {
+        String s = "";
+        for (Iterator<Placemark> iter = placemarks.iterator(); iter.hasNext(); ) {
+            Placemark p = (Placemark) iter.next();
+            s += p.getTitle() + "\n" + p.getDescription() + "\n\n";
+        }
+        return s;
+    }
+
+    public void addCurrentPlacemark() {
+        placemarks.add(currentPlacemark);
+    }
+
+    public ArrayList<Placemark> getPlacemarks() {
+        return placemarks;
+    }
+
+    public void setPlacemarks(ArrayList<Placemark> placemarks) {
+        this.placemarks = placemarks;
+    }
+
+    public Placemark getCurrentPlacemark() {
+        return currentPlacemark;
+    }
+
+    public void setCurrentPlacemark(Placemark currentPlacemark) {
+        this.currentPlacemark = currentPlacemark;
+    }
+
+    public void setRoutePlacemark(Placemark routePlacemark) {
+        this.routePlacemark = routePlacemark;
+    }
+}
